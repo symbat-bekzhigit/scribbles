6a842ddd823853e40adbb1874292d43fb5c05582
Sanjana-Nambiar <sanjuvn1@gmail.com>
2024-04-29 20:47:45 +0400

    initial commit

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,73 @@
+import "package:flutter/material.dart";
+import "package:messaging_app/services/auth/auth_service.dart";
+import "package:messaging_app/pages/settings_page.dart";
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  void logout(){
+    final auth = AuthService();
+    auth.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              //logo 
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 40,
+                  )
+                )
+              ),
+
+              //home list title
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("H O M E"),
+                  leading: const Icon(Icons.home),
+                  onTap: () {
+                    Navigator.pop(context);
+                  }
+                ),
+              ),
+
+              //settings list title
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("S E T T I N G S"),
+                  leading: const Icon(Icons.settings),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingsPage()));
+                  }
+                ),
+              ),
+            ],
+          ),
+          
+          //logout list title
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 20.0),
+            child: ListTile(
+              title: const Text("L O G O U T"),
+              leading: const Icon(Icons.logout),
+              onTap: logout
+            ),
+          )
+
+      ],)
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,73 @@
+import "package:flutter/material.dart";
+import "package:messaging_app/components/my_drawer.dart";
+import "package:messaging_app/components/user_title.dart";
+import "package:messaging_app/pages/chat_page.dart";
+import "package:messaging_app/services/auth/auth_service.dart";
+import "package:messaging_app/services/chat/chat_service.dart";
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Scribbles"),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.grey,
+        elevation: 0
+        ),
+      drawer: const MyDrawer(),
+      body: _buildUserList()
+    );
+  }
+
+  // build a list of users except for the current logged in user
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatService.getUsersStream(),
+      builder: (context, snapshot) {
+        // error
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        // loading..
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading...");
+        }
+
+        // return list view
+        return ListView(
+          children: snapshot.data!
+          .map<Widget>((userData) => _buildUserListItem(userData, context))
+          .toList(),
+        ); // ListView
+      },
+    ); // StreamBuilder
+  }
+
+  // build individual list tile for user
+  Widget _buildUserListItem(Map<String, dynamic> userData, BuildContext context) {
+    if (userData["email"] != _authService.getCurrentUser()!.email) { 
+      return UserTile(
+        text: userData["email"],
+        onTap: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatPage(
+                receiverEmail: userData["email"], receiverID: userData["uid"],
+              ),
+            ),
+          );
+        }
+      );
+    }else{
+      return Container();
+    }
+  }
+}
