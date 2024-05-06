c4e485ae6ee3dffa6cc1e33397287fbb4db0437a
Symbat Bekzhigit <sb7971@nyu.edu>
2024-05-07 00:29:50 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,27 @@ import 'package:messaging_app/firebase_options.dart';
 import 'package:messaging_app/themes/light_mode.dart';
 import 'package:flutter/material.dart';
 
+import 'package:messaging_app/services/paging/paging_service.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import "package:messaging_app/services/notification/notification_service_paging.dart";
+
+
+
+// Define a global navigator key
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  debugPrint("Handling a background message: ${message.messageId}");
+}
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+
+
   runApp(const MyApp());
 }
 
@@ -17,6 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       home: const SplashPage(),
       theme:lightMode,
